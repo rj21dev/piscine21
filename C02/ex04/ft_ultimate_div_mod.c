578b4049a5c352132e0ce47a79dd0a57144ccf36
Oleg <oleg.fomin369@yandex.ru>
2021-08-19 00:19:01 +0300

    del

--- C02/ex04/ft_ultimate_div_mod.c
+++
@@ -1,8 +0,0 @@
-void	ft_ultimate_div_mod(int *a, int *b)
-{
-	int	tmp;
-
-	tmp = *a;
-	*a = *a / *b;
-	*b = tmp % *b;
-}
--- C02/ex06/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int	ft_strlen(char *str);
-
-int	main(void)
-{
-	char str[] = "abcdqwerty";
-	printf("str = \"%s\", length = %d\n", str, ft_strlen(str));
-}
