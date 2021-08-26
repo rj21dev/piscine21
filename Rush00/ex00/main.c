adc9df507de90b9c9a8a74e5c0a9fa06d50df405
Oleg <oleg.fomin369@yandex.ru>
2021-08-26 23:25:08 +0300

    all done

---
+++ Rush00/ex00/main.c
@@ -0,0 +1,18 @@
+void	ft_putchar(char c);
+
+void	rush(int x, int y);
+
+int	main(void)
+{
+	rush(5, 3);
+	ft_putchar('\n');
+	rush(5, 1);
+	ft_putchar('\n');
+	rush(1, 1);
+	ft_putchar('\n');
+	rush(1, 5);
+	ft_putchar('\n');
+	rush(4, 4);
+	ft_putchar('\n');
+	return (0);
+}
